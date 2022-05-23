92875adfe35e6c02b6a9dd59e735f766857f6bc2
Swimotter <mr.jacksonrubiano@gmail.com>
2022-05-22 23:31:59 -0700

    Added deconstructor and copy constructors

--- OtterEngine/otterVector.h
+++ OtterEngine/otterVector.h
@@ -3,12 +3,20 @@ namespace otterEngine {
 	private:
 
 	public:
-		//Position in cartesian coordinates
+		//position in cartesian coordinates
 		float x;
 		float y;
 		float z;
 
+		//constructors
 		otterVector();
 		otterVector(float _x, float_y, float _z);
+
+		//deconstructor
+		~otterVector();
+
+		//copy constructors
+		otterVector(const otterVector& v);
+		otterVector& operator=(const otterVector& v);
 	};
 }
