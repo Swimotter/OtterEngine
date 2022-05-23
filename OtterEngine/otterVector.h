523f65fcdb53f7054f3501cf950d1b926b67a1e9
Swimotter <mr.jacksonrubiano@gmail.com>
2022-05-22 23:28:13 -0700

    Added constructors and public variables

--- OtterEngine/otterVector.h
+++ OtterEngine/otterVector.h
@@ -1,3 +1,14 @@
 namespace otterEngine {
+	class otterVector {
+	private:
 
+	public:
+		//Position in cartesian coordinates
+		float x;
+		float y;
+		float z;
+
+		otterVector();
+		otterVector(float _x, float_y, float _z);
+	};
 }
