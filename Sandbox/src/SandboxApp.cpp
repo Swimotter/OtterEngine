cc0064a44410ac023e39040184966cb0dd6c9897
Swimotter <mr.jacksonrubiano@gmail.com>
2022-06-28 20:16:32 -0700

    Add temp creation of app

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -1,1 +1,14 @@
 #include <Otter.h>
+
+class Sandbox : public Otter::Application {
+public:
+	Sandbox() {}
+
+	~Sandbox() {}
+};
+
+void main() {
+	Sandbox* sandbox = new Sandbox();
+	sandbox->Run();
+	delete sandbox;
+}
