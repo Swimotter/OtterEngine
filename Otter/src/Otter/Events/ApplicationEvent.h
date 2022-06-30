0b4b628cf8b1f34f2d8be8a697bff9f3f071ceda
Swimotter <mr.jacksonrubiano@gmail.com>
2022-06-30 00:08:50 -0700

    Add app event header and window resize event

---
+++ Otter/src/Otter/Events/ApplicationEvent.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "Event.h"
+#include <sstream>
+
+namespace Otter {
+	class OTTER_APi WindowResizeEvent : public Event {
+	public:
+		WindowResizeEvent(unsigned int width, unsigned int height) : _Width(width), _Height(height) {}
+
+		inline unsigned int GetWidth() const { return _Width; }
+		inline unsigned int GetHeight() const { return _Height; }
+
+		std::string ToString() const override {
+			std::stringstream ss;
+			ss << "WindowResizeEvent: " << _Width << ", " << _Height;
+			return ss.str();
+		}
+
+		EVENT_CLASS_TYPE(WindowResize)
+			EVENT_CLASS_CATEGORY(EventCategoryApplication)
+	private:
+		unsigned int _Width, _Height;
+	};
+}
+
+WindowClose, WindowResize
