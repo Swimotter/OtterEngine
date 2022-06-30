39a2363c5824e6d5a867dde3a78429a5dfd2a647
Swimotter <mr.jacksonrubiano@gmail.com>
2022-06-30 00:28:15 -0700

    Add window resize test

--- Otter/src/Otter/Application.cpp
+++ Otter/src/Otter/Application.cpp
@@ -1,4 +1,6 @@
 #include "Application.h"
+#include "Otter/Events/ApplicationEvent.h"
+#include "Otter/Log.h"
 
 namespace Otter {
 
@@ -7,6 +9,8 @@ namespace Otter {
 	Application::~Application() {}
 
 	void Application::Run() {
+		WindowResizeEvent e(1200, 720);
+		OTTER_TRACE(e);
 		while (true);
 	}
 }
