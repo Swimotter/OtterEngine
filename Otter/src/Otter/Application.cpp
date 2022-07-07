39ae024286a5e5b7ed56d85b001c09545b2980d5
Swimotter <mr.jacksonrubiano@gmail.com>
2022-07-06 18:46:00 -0700

    Send layers one by one to the OnUpdate function

--- Otter/src/Otter/Application.cpp
+++ Otter/src/Otter/Application.cpp
@@ -35,6 +35,11 @@ namespace Otter {
 		while (_running) {
 			glClearColor(1, 0, 1, 1);
 			glClear(GL_COLOR_BUFFER_BIT);
+
+			for (Layer* layer : _layerStack) {
+				layer->OnUpdate();
+			}
+
 			_window->OnUpdate();
 		}
 	}
