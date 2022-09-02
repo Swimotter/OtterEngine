6e9add4c0922a7d992af7d8e3928cbc1615abf5c
Swimotter <mr.jacksonrubiano@gmail.com>
2022-09-02 01:48:07 -0700

    Add comments to MouseEvent

--- Otter/src/Otter/Events/MouseEvent.h
+++ Otter/src/Otter/Events/MouseEvent.h
@@ -1,9 +1,24 @@
 #pragma once
 
+/**
+* Handles all mouse events
+* @see Event.h
+*
+* @author Jackson Rubiano
+* @version 1.0
+* @since 0.0.0-pa.1
+*/
+
 #include "Event.h"
 
 namespace Otter {
 
+	/**
+	* Handles moving the mouse
+	*
+	* @param x, y These are the x and y coordinates of the mouse
+	* @return string This returns the mouse position
+	*/
 	class OTTER_API MouseMovedEvent : public Event {
 	public:
 		MouseMovedEvent(float x, float y) : _mouseX(x), _mouseY(y) {}
@@ -17,12 +32,19 @@ namespace Otter {
 			return ss.str();
 		}
 
+		//Event groups
 		EVENT_CLASS_TYPE(MouseMoved)
 		EVENT_CLASS_CATEGORY(EventCategoryMouse | EventCategoryInput)
 	private:
 		float _mouseX, _mouseY;
 	};
 
+	/**
+	* Handles mouse scrolling
+	*
+	* @param offsetX, offsetY These are the vertical and horizontal scrolling since the last frame
+	* @return string This returns the amount scrolled since last frame
+	*/
 	class OTTER_API MouseScrolledEvent : public Event {
 	public:
 		MouseScrolledEvent(float offsetX, float offsetY) : _offsetX(offsetX), _offsetY(offsetY) {}
@@ -36,15 +58,24 @@ namespace Otter {
 			return ss.str();
 		}
 
+		//Event groups
 		EVENT_CLASS_TYPE(MouseScrolled)
 		EVENT_CLASS_CATEGORY(EventCategoryMouse | EventCategoryInput)
 	private:
 		float _offsetX, _offsetY;
 	};
 
+	/**
+	* Passes mouse button number to MouseButtonPressedEvent and MouseButtonReleasedEvent
+	* @see MouseButtonPressedEvent
+	* @see MouseButtonReleasedEvent
+	* 
+	* @return int This returns the mouse button number
+	*/
 	class OTTER_API MouseButtonEvent : public Event {
 	public:
 		inline int GetMouseButton() const { return _button; }
+		//Event group
 		EVENT_CLASS_CATEGORY(EventCategoryMouse | EventCategoryInput)
 	protected:
 		MouseButtonEvent(int button) : _button(button) {}
@@ -52,6 +83,13 @@ namespace Otter {
 		int _button;
 	};
 
+	/**
+	* Handles mouse button presses
+	* @see MouseButtonEvent
+	*
+	* @param button This is the numerical id of the mouse button
+	* @return string This returns the mouse button pressed in numerical form
+	*/
 	class OTTER_API MouseButtonPressedEvent : public MouseButtonEvent {
 	public:
 		MouseButtonPressedEvent(int button) : MouseButtonEvent(button) {}
@@ -62,9 +100,17 @@ namespace Otter {
 			return ss.str();
 		}
 
+		//Event group
 		EVENT_CLASS_TYPE(MouseButtonPressed)
 	};
 
+	/**
+	* Handles mouse button releases
+	* @see MouseButtonEvent
+	*
+	* @param button This is the numerical id of the mouse button
+	* @return string This returns the mouse button released in numerical form
+	*/
 	class OTTER_API MouseButtonReleasedEvent : public MouseButtonEvent {
 	public:
 		MouseButtonReleasedEvent(int button) : MouseButtonEvent(button) {}
@@ -75,6 +121,7 @@ namespace Otter {
 			return ss.str();
 		}
 
+		//Event group
 		EVENT_CLASS_TYPE(MouseButtonReleased)
 	};
 }
