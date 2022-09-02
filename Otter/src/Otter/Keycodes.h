32bf292cee1c94c84828986e7539125895d3518c
Swimotter <mr.jacksonrubiano@gmail.com>
2022-09-02 02:59:15 -0700

    Add comments to Keycodes

--- Otter/src/Otter/Keycodes.h
+++ Otter/src/Otter/Keycodes.h
@@ -1,124 +1,132 @@
 #pragma once
 
-#define OTTER_KEY_SPACE              32
-#define OTTER_KEY_APOSTROPHE         39  /* ' */
-#define OTTER_KEY_COMMA              44  /* , */
-#define OTTER_KEY_MINUS              45  /* - */
-#define OTTER_KEY_PERIOD             46  /* . */
-#define OTTER_KEY_SLASH              47  /* / */
-#define OTTER_KEY_0                  48
-#define OTTER_KEY_1                  49
-#define OTTER_KEY_2                  50
-#define OTTER_KEY_3                  51
-#define OTTER_KEY_4                  52
-#define OTTER_KEY_5                  53
-#define OTTER_KEY_6                  54
-#define OTTER_KEY_7                  55
-#define OTTER_KEY_8                  56
-#define OTTER_KEY_9                  57
-#define OTTER_KEY_SEMICOLON          59  /* ; */
-#define OTTER_KEY_EQUAL              61  /* = */
-#define OTTER_KEY_A                  65
-#define OTTER_KEY_B                  66
-#define OTTER_KEY_C                  67
-#define OTTER_KEY_D                  68
-#define OTTER_KEY_E                  69
-#define OTTER_KEY_F                  70
-#define OTTER_KEY_G                  71
-#define OTTER_KEY_H                  72
-#define OTTER_KEY_I                  73
-#define OTTER_KEY_J                  74
-#define OTTER_KEY_K                  75
-#define OTTER_KEY_L                  76
-#define OTTER_KEY_M                  77
-#define OTTER_KEY_N                  78
-#define OTTER_KEY_O                  79
-#define OTTER_KEY_P                  80
-#define OTTER_KEY_Q                  81
-#define OTTER_KEY_R                  82
-#define OTTER_KEY_S                  83
-#define OTTER_KEY_T                  84
-#define OTTER_KEY_U                  85
-#define OTTER_KEY_V                  86
-#define OTTER_KEY_W                  87
-#define OTTER_KEY_X                  88
-#define OTTER_KEY_Y                  89
-#define OTTER_KEY_Z                  90
-#define OTTER_KEY_LEFT_BRACKET       91  /* [ */
-#define OTTER_KEY_BACKSLASH          92  /* \ */
-#define OTTER_KEY_RIGHT_BRACKET      93  /* ] */
-#define OTTER_KEY_GRAVE_ACCENT       96  /* ` */
-#define OTTER_KEY_WORLD_1            161 /* non-US #1 */
-#define OTTER_KEY_WORLD_2            162 /* non-US #2 */
+/**
+* Keycodes for keyboard buttons
+*
+* @author Jackson Rubiano
+* @version 1.0
+* @since 0.0.0-pa.1
+*/
 
-/* Function keys */
-#define OTTER_KEY_ESCAPE             256
-#define OTTER_KEY_ENTER              257
-#define OTTER_KEY_TAB                258
-#define OTTER_KEY_BACKSPACE          259
-#define OTTER_KEY_INSERT             260
-#define OTTER_KEY_DELETE             261
-#define OTTER_KEY_RIGHT              262
-#define OTTER_KEY_LEFT               263
-#define OTTER_KEY_DOWN               264
-#define OTTER_KEY_UP                 265
-#define OTTER_KEY_PAGE_UP            266
-#define OTTER_KEY_PAGE_DOWN          267
-#define OTTER_KEY_HOME               268
-#define OTTER_KEY_END                269
-#define OTTER_KEY_CAPS_LOCK          280
-#define OTTER_KEY_SCROLL_LOCK        281
-#define OTTER_KEY_NUM_LOCK           282
-#define OTTER_KEY_PRINT_SCREEN       283
-#define OTTER_KEY_PAUSE              284
-#define OTTER_KEY_F1                 290
-#define OTTER_KEY_F2                 291
-#define OTTER_KEY_F3                 292
-#define OTTER_KEY_F4                 293
-#define OTTER_KEY_F5                 294
-#define OTTER_KEY_F6                 295
-#define OTTER_KEY_F7                 296
-#define OTTER_KEY_F8                 297
-#define OTTER_KEY_F9                 298
-#define OTTER_KEY_F10                299
-#define OTTER_KEY_F11                300
-#define OTTER_KEY_F12                301
-#define OTTER_KEY_F13                302
-#define OTTER_KEY_F14                303
-#define OTTER_KEY_F15                304
-#define OTTER_KEY_F16                305
-#define OTTER_KEY_F17                306
-#define OTTER_KEY_F18                307
-#define OTTER_KEY_F19                308
-#define OTTER_KEY_F20                309
-#define OTTER_KEY_F21                310
-#define OTTER_KEY_F22                311
-#define OTTER_KEY_F23                312
-#define OTTER_KEY_F24                313
-#define OTTER_KEY_F25                314
-#define OTTER_KEY_KP_0               320
-#define OTTER_KEY_KP_1               321
-#define OTTER_KEY_KP_2               322
-#define OTTER_KEY_KP_3               323
-#define OTTER_KEY_KP_4               324
-#define OTTER_KEY_KP_5               325
-#define OTTER_KEY_KP_6               326
-#define OTTER_KEY_KP_7               327
-#define OTTER_KEY_KP_8               328
-#define OTTER_KEY_KP_9               329
-#define OTTER_KEY_KP_DECIMAL         330
-#define OTTER_KEY_KP_DIVIDE          331
-#define OTTER_KEY_KP_MULTIPLY        332
-#define OTTER_KEY_KP_SUBTRACT        333
-#define OTTER_KEY_KP_ADD             334
-#define OTTER_KEY_KP_ENTER           335
-#define OTTER_KEY_KP_EQUAL           336
-#define OTTER_KEY_LEFT_SHIFT         340
-#define OTTER_KEY_LEFT_CONTROL       341
-#define OTTER_KEY_LEFT_ALT           342
-#define OTTER_KEY_LEFT_SUPER         343
-#define OTTER_KEY_RIGHT_SHIFT        344
-#define OTTER_KEY_RIGHT_CONTROL      345
-#define OTTER_KEY_RIGHT_ALT          346
-#define OTTER_KEY_RIGHT_SUPER        347
-#define OTTER_KEY_MENU               348
+#define OTTER_KEY_SPACE				32	/*   */
+#define OTTER_KEY_APOSTROPHE		39	/* ' */
+#define OTTER_KEY_COMMA				44	/* , */
+#define OTTER_KEY_MINUS				45	/* - */
+#define OTTER_KEY_PERIOD			46	/* . */
+#define OTTER_KEY_SLASH				47	/* / */
+#define OTTER_KEY_0					48	/* 0 */
+#define OTTER_KEY_1					49	/* 1 */
+#define OTTER_KEY_2					50	/* 2 */
+#define OTTER_KEY_3					51	/* 3 */
+#define OTTER_KEY_4					52	/* 4 */
+#define OTTER_KEY_5					53	/* 5 */
+#define OTTER_KEY_6					54	/* 6 */
+#define OTTER_KEY_7					55	/* 7 */
+#define OTTER_KEY_8					56	/* 8 */
+#define OTTER_KEY_9					57	/* 9 */
+#define OTTER_KEY_SEMICOLON			59	/* ; */
+#define OTTER_KEY_EQUAL				61	/* = */
+#define OTTER_KEY_A					65	/* A */
+#define OTTER_KEY_B					66	/* B */
+#define OTTER_KEY_C					67	/* C */
+#define OTTER_KEY_D					68	/* D */
+#define OTTER_KEY_E					69	/* E */
+#define OTTER_KEY_F					70	/* F */
+#define OTTER_KEY_G					71	/* G */
+#define OTTER_KEY_H					72	/* H */
+#define OTTER_KEY_I					73	/* I */
+#define OTTER_KEY_J					74	/* J */
+#define OTTER_KEY_K					75	/* K */
+#define OTTER_KEY_L					76	/* L */
+#define OTTER_KEY_M					77	/* M */
+#define OTTER_KEY_N					78	/* N */
+#define OTTER_KEY_O					79	/* O */
+#define OTTER_KEY_P					80	/* P */
+#define OTTER_KEY_Q					81	/* Q */
+#define OTTER_KEY_R					82	/* R */
+#define OTTER_KEY_S					83	/* S */
+#define OTTER_KEY_T					84	/* T */
+#define OTTER_KEY_U					85	/* U */
+#define OTTER_KEY_V					86	/* V */
+#define OTTER_KEY_W					87	/* W */
+#define OTTER_KEY_X					88	/* X */
+#define OTTER_KEY_Y					89	/* Y */
+#define OTTER_KEY_Z					90	/* Z */
+#define OTTER_KEY_LEFT_BRACKET		91	/* [ */
+#define OTTER_KEY_BACKSLASH			92	/* \ */
+#define OTTER_KEY_RIGHT_BRACKET		93	/* ] */
+#define OTTER_KEY_GRAVE_ACCENT		96	/* ` */
+#define OTTER_KEY_WORLD_1			161 /* non-US #1 */
+#define OTTER_KEY_WORLD_2			162 /* non-US #2 */
+
+//Function keys
+#define OTTER_KEY_ESCAPE			256
+#define OTTER_KEY_ENTER				257
+#define OTTER_KEY_TAB				258
+#define OTTER_KEY_BACKSPACE			259
+#define OTTER_KEY_INSERT			260
+#define OTTER_KEY_DELETE			261
+#define OTTER_KEY_RIGHT				262
+#define OTTER_KEY_LEFT				263
+#define OTTER_KEY_DOWN				264
+#define OTTER_KEY_UP				265
+#define OTTER_KEY_PAGE_UP			266
+#define OTTER_KEY_PAGE_DOWN			267
+#define OTTER_KEY_HOME				268
+#define OTTER_KEY_END				269
+#define OTTER_KEY_CAPS_LOCK			280
+#define OTTER_KEY_SCROLL_LOCK		281
+#define OTTER_KEY_NUM_LOCK			282
+#define OTTER_KEY_PRINT_SCREEN		283
+#define OTTER_KEY_PAUSE				284
+#define OTTER_KEY_F1				290
+#define OTTER_KEY_F2				291
+#define OTTER_KEY_F3				292
+#define OTTER_KEY_F4				293
+#define OTTER_KEY_F5				294
+#define OTTER_KEY_F6				295
+#define OTTER_KEY_F7				296
+#define OTTER_KEY_F8				297
+#define OTTER_KEY_F9				298
+#define OTTER_KEY_F10				299
+#define OTTER_KEY_F11				300
+#define OTTER_KEY_F12				301
+#define OTTER_KEY_F13				302
+#define OTTER_KEY_F14				303
+#define OTTER_KEY_F15				304
+#define OTTER_KEY_F16				305
+#define OTTER_KEY_F17				306
+#define OTTER_KEY_F18				307
+#define OTTER_KEY_F19				308
+#define OTTER_KEY_F20				309
+#define OTTER_KEY_F21				310
+#define OTTER_KEY_F22				311
+#define OTTER_KEY_F23				312
+#define OTTER_KEY_F24				313
+#define OTTER_KEY_F25				314
+#define OTTER_KEY_KP_0				320
+#define OTTER_KEY_KP_1				321
+#define OTTER_KEY_KP_2				322
+#define OTTER_KEY_KP_3				323
+#define OTTER_KEY_KP_4				324
+#define OTTER_KEY_KP_5				325
+#define OTTER_KEY_KP_6				326
+#define OTTER_KEY_KP_7				327
+#define OTTER_KEY_KP_8				328
+#define OTTER_KEY_KP_9				329
+#define OTTER_KEY_KP_DECIMAL		330
+#define OTTER_KEY_KP_DIVIDE			331
+#define OTTER_KEY_KP_MULTIPLY		332
+#define OTTER_KEY_KP_SUBTRACT		333
+#define OTTER_KEY_KP_ADD			334
+#define OTTER_KEY_KP_ENTER			335
+#define OTTER_KEY_KP_EQUAL			336
+#define OTTER_KEY_LEFT_SHIFT		340
+#define OTTER_KEY_LEFT_CONTROL		341
+#define OTTER_KEY_LEFT_ALT			342
+#define OTTER_KEY_LEFT_SUPER		343
+#define OTTER_KEY_RIGHT_SHIFT		344
+#define OTTER_KEY_RIGHT_CONTROL		345
+#define OTTER_KEY_RIGHT_ALT			346
+#define OTTER_KEY_RIGHT_SUPER		347
+#define OTTER_KEY_MENU				348
