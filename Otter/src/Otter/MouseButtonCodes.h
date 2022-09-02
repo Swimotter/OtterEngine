8754db6b56a20140aec295f74d978ba45925847d
Swimotter <mr.jacksonrubiano@gmail.com>
2022-09-02 03:44:45 -0700

    Add comments to MouseButtonCodes

--- Otter/src/Otter/MouseButtonCodes.h
+++ Otter/src/Otter/MouseButtonCodes.h
@@ -1,10 +1,18 @@
 #pragma once
 
-#define OTTER_MOUSE_BUTTON_1         0
-#define OTTER_MOUSE_BUTTON_2         1
-#define OTTER_MOUSE_BUTTON_3         2
-#define OTTER_MOUSE_BUTTON_4         3
-#define OTTER_MOUSE_BUTTON_5         4
-#define OTTER_MOUSE_BUTTON_6         5
-#define OTTER_MOUSE_BUTTON_7         6
-#define OTTER_MOUSE_BUTTON_8         7
+/**
+* Codes for mouse buttons
+*
+* @author Jackson Rubiano
+* @version 1.0
+* @since 0.0.0-pa.1
+*/
+
+#define OTTER_MOUSE_BUTTON_1		0
+#define OTTER_MOUSE_BUTTON_2		1
+#define OTTER_MOUSE_BUTTON_3		2
+#define OTTER_MOUSE_BUTTON_4		3
+#define OTTER_MOUSE_BUTTON_5		4
+#define OTTER_MOUSE_BUTTON_6		5
+#define OTTER_MOUSE_BUTTON_7		6
+#define OTTER_MOUSE_BUTTON_8		7
