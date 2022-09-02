b44903e77502c61b15bb22923de92559e0d11b56
Swimotter <mr.jacksonrubiano@gmail.com>
2022-09-02 02:28:51 -0700

    Add comments to ApplicationEvent

--- Otter/src/Otter/Events/ApplicationEvent.h
+++ Otter/src/Otter/Events/ApplicationEvent.h
@@ -1,9 +1,24 @@
 #pragma once
 
+/**
+* Handles all application events
+* @see Event.h
+*
+* @author Jackson Rubiano
+* @version 1.0
+* @since 0.0.0-pa.1
+*/
+
 #include "Event.h"
 
 namespace Otter {
 
+	/**
+	* Handles resizing application 
+	*
+	* @param width, height These are the width and height of the application window
+	* @return string This is dimensions of the application window
+	*/
 	class OTTER_API WindowResizeEvent : public Event {
 	public:
 		WindowResizeEvent(unsigned int width, unsigned int height) : _width(width), _height(height) {}
@@ -17,16 +32,21 @@ namespace Otter {
 			return ss.str();
 		}
 
+		//Event groups
 		EVENT_CLASS_TYPE(WindowResize)
 		EVENT_CLASS_CATEGORY(EventCategoryApplication)
 	private:
 		unsigned int _width, _height;
 	};
 
+	/**
+	* Handles closing application
+	*/
 	class OTTER_API WindowCloseEvent : public Event {
 	public:
 		WindowCloseEvent() {}
 
+		//Event groups
 		EVENT_CLASS_TYPE(WindowClose)
 		EVENT_CLASS_CATEGORY(EventCategoryApplication)
 	};
